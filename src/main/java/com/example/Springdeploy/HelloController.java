60800b6823943184077e23217e28370cbcaa1993
Andres Sepulveda <bodyweightforce@gmail.com>
2022-10-15 11:48:27 -0300

    Initial commit

---
+++ src/main/java/com/example/Springdeploy/HelloController.java
@@ -0,0 +1,36 @@
+package com.example.Springdeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @Value("${app.message}")
+    String message;
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        System.out.println(message);
+        return "Hola Mundo, actualizando metodos nuevamente";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap() {
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                  </head>
+                  <body>
+                    <h1>Hola mundo desde Spring Boot!</h1>
+                  </body>
+                </html>
+                """;
+    }
+}
